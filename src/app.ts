67839611200ee46ac076ea435806ed25f16ab868
ntduc <ntduc6@cmc.com.vn>
2021-09-29 17:38:18 +0700

    write unit test readings data

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,23 @@ import { recommend, compare } from "./price-plans/price-plans-controller";
 
 export const app = express();
 
-const { getReadings, setReadings } = readings(readingsData);
+// const { getReadings, setReadings } = readings(readingsData);
 
-app.get("/readings/read/:smartMeterId", (req, res) => {
-    res.send(read(getReadings, req));
-});
+// app.get("/readings/read/:smartMeterId", (req, res) => {
+//     res.send(read(getReadings, req));
+// });
 
-app.post("/readings/store", (req, res) => {
-    res.send(store(setReadings, req));
-});
+// app.post("/readings/store", (req, res) => {
+//     res.send(store(setReadings, req));
+// });
 
-app.get("/price-plans/recommend/:smartMeterId", (req, res) => {
-    res.send(recommend(getReadings, req));
-});
+// app.get("/price-plans/recommend/:smartMeterId", (req, res) => {
+//     res.send(recommend(getReadings, req));
+// });
 
-app.get("/price-plans/compare-all/:smartMeterId", (req, res) => {
-    res.send(compare(getReadings, req));
-});
+// app.get("/price-plans/compare-all/:smartMeterId", (req, res) => {
+//     res.send(compare(getReadings, req));
+// });
 
 
 const port = process.env.PORT || 4000;
--- src/readings/readings.ts
+++ src/readings/readings.ts
@@ -1,1 +1,3 @@
-export const readings = () => {};
+export const readings = () => {
+
+};
